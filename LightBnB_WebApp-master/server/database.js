e454e18a83aab2f75cc99f65256bd27dc4d2e2c2
robinwebber <robinewebber@gmail.com>
2019-07-25 13:54:28 +0000

    buggy app for review

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'robin',
+  password: 'robin',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
 /// Users
 
 /**
@@ -67,11 +76,14 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  pool.query(`
+  SELECT * FROM properties
+  LIMIT $1
+  `, [limit])
+  .then(res => {
+    console.log(res.rows)
+  })
+  .catch(err);
 }
 exports.getAllProperties = getAllProperties;
 
